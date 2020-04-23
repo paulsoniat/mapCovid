c121416645ccd911feecd1bf1cc48fb91509022e
paulsoniat <paulmsoniat@gmail.com>
2020-04-23 13:29:02 -0500

    [init] Random map data appeappearing in main page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Component } from "react";
+import ReactTooltip from "react-tooltip";
+
+import "./index.css";
+
+import MapChart from "./MapChart";
+
+import axios from 'axios';
 
 function App() {
+  const [content, setContent] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MapChart setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
     </div>
   );
 }
